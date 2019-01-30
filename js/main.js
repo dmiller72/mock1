b945b5fd5f71a1afbdcdf64ba5c8b6ed35f48797
David Miller <dmiller@mtechenterprises.com>
2019-01-30 03:22:48 -0600

    Added smooth scrolling and about section works

--- js/main.js
+++ js/main.js
@@ -9,3 +9,41 @@ const slide = ()=> {
 }
 
 slide();
+
+
+//*** */ about us text****//
+
+const about = () => {
+    const paraOne  = document.getElementById("para-one");
+    const paraTwo  = document.getElementById("para-two");
+    const paraThree  = document.getElementById("para-three");
+    const mission = document.querySelector(".mission");
+    const value = document.querySelector(".value");
+    const vision = document.querySelector(".vision");
+    mission.addEventListener('click', () => {
+        mission.classList.add("active");
+        value.classList.remove("active");
+        vision.classList.remove("active");
+        paraOne.classList.remove('zero');
+        paraTwo.classList.add('zero');
+        paraThree.classList.add('zero');
+    });
+    value.addEventListener('click', () => {
+        value.classList.add("active");
+        mission.classList.remove("active");
+        vision.classList.remove("active");
+        paraOne.classList.add('zero');
+        paraTwo.classList.remove('zero');
+        paraThree.classList.add('zero');
+    });
+     vision.addEventListener('click', () => {
+        vision.classList.add("active");
+        mission.classList.remove("active");
+        value.classList.remove("active");
+        paraOne.classList.add('zero');
+        paraTwo.classList.add('zero');
+        paraThree.classList.remove('zero');
+    });
+}
+
+about();
